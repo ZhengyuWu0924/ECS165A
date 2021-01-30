0109e39af9e026a5360147a2e9e03da59e079740
ZhengyuWu0924 <zywwu@ucdavis.edu>
2021-01-29 20:04:45 -0800

    doc(db_tester):database tester
    tester file to test features in database

---
+++ 165aDataBase/165a-winter-2021-main/db_test.py
@@ -0,0 +1,24 @@
+from template.db import Database
+
+database = Database()
+grades_table = database.create_table('Grades', 5, 0)
+
+# retValue = database.get_table('Grades')
+# print(retValue)
+# db_length = len(database.tables_directory)
+# print(db_length)
+
+sec_grades_table = database.create_table('Grades2', 5, 0)
+retNewTable = database.get_table('Grades2')
+print(retNewTable)
+db_length2 = len(database.tables_directory)
+print("Data base length:",db_length2)
+print("Table name:",sec_grades_table.name)
+print("Table key",sec_grades_table.Table_key)
+print("Table columns:",sec_grades_table.num_columns)
+print("Table page directory:",sec_grades_table.page_directory)
+print("Table index:",sec_grades_table.index)
+print("Table record directory:",sec_grades_table.record_directory)
+print("Table prange:",sec_grades_table.prange_num)
+print("Table free base rid:",sec_grades_table.free_brid)
+print("Table free tail rid:",sec_grades_table.free_trid)
