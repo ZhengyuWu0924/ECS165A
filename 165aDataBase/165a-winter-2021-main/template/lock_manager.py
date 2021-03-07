7971972ab7c5fc2140aae9895e8534b7f8980d01
TTTTTTedder <cchlao@ucdavis.edu>
2021-03-08 01:30:47 +0800

    lock

---
+++ 165aDataBase/165a-winter-2021-main/template/lock_manager.py
@@ -0,0 +1,20 @@
+# -* -coding:utf-8 -*-
+# @Time:2021/3/8 1:29 上午
+# @Author: Tedder Lao
+
+
+import threading
+
+
+class LockingCounter():
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.count = 0
+
+    def increment(self):
+        with self.lock:
+        self.count += 1
+
+
+
