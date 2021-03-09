1b22c94bbac7a1edba37866955f8ef51ee48afeb
ZhengyuWu0924 <zywwu@ucdavis.edu>
2021-03-08 17:20:33 -0800

    update(log)

--- 165aDataBase/165a-winter-2021-main/template/log.py
+++ 165aDataBase/165a-winter-2021-main/template/log.py
@@ -1,30 +1,41 @@
 from template.transaction_worker import TransactionWorker
+from template.config import *
 class Log:
     def __init__(self, transaction):
         # transactionNumber = [TransactionWorker1, 2, 3, 4 ... 8]
         # information = [] // one record per line
         # eg. information = [RID, Tn, [0,0,0,0,0]]
         # status = Start or Commit
+        self.status = LOG_START
+        
+
+    def readLog(self):
+        return True
+    
+    def writeLog(self):
+        return True
+    
+    def rollBack(self):
 
 
-functions:
+# functions:
 
-1. read Log:
-    read permanent file into memory
-    return true while success
+# 1. read Log:
+#     read permanent file into memory
+#     return true while success
 
-2. write log:
-    when a transaction commit (called in transaction class)
-    the last log state = commit
-    write the current in memory log file into file disk
-    read the log file from disk (can skip this step?) to make sure it is the correct latest version
-    return true while success
+# 2. write log:
+#     when a transaction commit (called in transaction class)
+#     the last log state = commit
+#     write the current in memory log file into file disk
+#     read the log file from disk (can skip this step?) to make sure it is the correct latest version
+#     return true while success
 
-3. roll back:
-    find the latest COMMIT status record in the log
-    return that record
+# 3. roll back:
+#     find the latest COMMIT status record in the log
+#     return that record
 
-4. record history: (necessary ???)
-    create a log for the record,
-    set status of this log to Start
-    append into the log list.
+# 4. record history: (necessary ???)
+#     create a log for the record,
+#     set status of this log to Start
+#     append into the log list.
