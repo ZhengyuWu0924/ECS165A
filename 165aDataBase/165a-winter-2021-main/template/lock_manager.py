66facabb370aed3249fe075d8c8b691652f3a851
ZhengyuWu0924 <zywwu@ucdavis.edu>
2021-03-10 16:01:26 -0800

    update(lock)

--- 165aDataBase/165a-winter-2021-main/template/lock_manager.py
+++ 165aDataBase/165a-winter-2021-main/template/lock_manager.py
@@ -1,20 +1,31 @@
-# -* -coding:utf-8 -*-
-# @Time:2021/3/8 1:29 上午
-# @Author: Tedder Lao
+from config import *
+from template.table import Record
 
+class Lock:
+    def __init__(self, mode, *record):
 
-import threading
+    def addLock():
+        # detect mutex or shared
+        return
+    def mutexLock():
+        # add mutex lock
+        return
+    def sharedLock():
+        # add shared lock
+        return
+    def releaseMutexLock():
+        # release record mutex lock 
+        return
+    def releaseSharedLock():
+        # release recird mutex lock
+        return
+    # @param: operation mode
+    # @param: record
+    def check():
+        # detect if any lock
+        # return if able to update / read the record
+        return
 
-
-class LockingCounter():
-
-    def __init__(self):
-        self.lock = threading.Lock()
-        self.count = 0
-
-    def increment(self):
-        with self.lock:
-            self.count += 1
 
 
 
