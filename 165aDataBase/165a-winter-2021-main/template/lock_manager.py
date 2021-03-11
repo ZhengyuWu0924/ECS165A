3ef865e02bd63a8298b24e523966a7722960cbe3
ZhengyuWu0924 <zywwu@ucdavis.edu>
2021-03-10 16:53:02 -0800

    Update lock_manager.py

--- 165aDataBase/165a-winter-2021-main/template/lock_manager.py
+++ 165aDataBase/165a-winter-2021-main/template/lock_manager.py
@@ -9,27 +9,80 @@ class Lock:
 
     def addLock(self, mode, *record):
         # detect mutex or shared
-        return
-    def _mutexLock(self, *record):
-        # add mutex lock
-        return
-    def _sharedLock(self, *record):
-        # add shared lock
-        return
-    def releaseLock(self, mode, *record):
-        return
-    def _releaseMutexLock(self, *record):
-        # release record mutex lock 
-        return
-    def _releaseSharedLock(self, *record):
-        # release recird mutex lock
-        return
+        status = False
+        if len(record) == 0:
+            return status
+        if mode == LOCK_MUTEX:
+            status = self._mutexLock(record[0])
+        if mode == LOCK_SHARED:
+            status = self._sharedLock(record[0])
+        return status
+    
+    # func: add mutex lock 
+    def _mutexLock(self, record):
+        # record has already been locked
+        if record.lock_amt > 0:
+            return False
+        # set mutex lock and increase amount
+        record.lock_mode = LOCK_MUTEX
+        record.lock_amt += 1
+        return True
+    
+    # func: add shared lock 
+    def _sharedLock(self, record):
+        if record.lock_amt > 0:
+            if record.lock_mode == LOCK_MUTEX:
+                return False
+            else:
+                record.lock_amt += 1
+        record.lock_mode = LOCK_SHARED
+        record.lock_amt += 1
+        return True
+
+    # func: release a lock 
+    def releaseLock(self, mode, record):
+        status = False
+        if len(record) == 0:
+            return status
+        if mode == LOCK_MUTEX:
+            status = self._releaseMutexLock(record[0])
+        elif mode == LOCK_SHARED:
+            status = self._releaseSharedLock(record[0])
+        return status
+    
+    # func: release mutex lock for a record
+    def _releaseMutexLock(self, record):
+        if record.lock_mode != LOCK_MUTEX:
+            return False
+        if record.lock_amt == 0:
+            return False
+        record.lock_mode = LOCK_UNLOCK
+        record.lock_amt = 0
+        return True
+    
+    # func: release shared lock for a record
+    def _releaseSharedLock(self, record):
+        if record.lock_mode != LOCK_SHARED:
+            return False
+        record.lock_amt -= 1
+        if record.lock_amt == 0:
+            record.lock_mode = LOCK_UNLOCK
+        
+        return True
+    
+    # func: detect if any lock
     # @param: operation mode
     # @param: record
-    def check(self, *record):
-        # detect if any lock
-        # return if able to update / read the record
-        return
+    def check(self, mode, *record):
+        if mode == LOCK_MUTEX:
+            if record[0].lock_mode == LOCK_UNLOCK and record[0].amt == 0:
+                return True
+            
+        if mode == LOCK_SHARED:
+            if record[0].lock_mode != LOCK_MUTEX:
+                return True
+            
+        return False
 
 
 
